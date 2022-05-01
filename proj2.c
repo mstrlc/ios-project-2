dca361268d1b369359d87a9db831d19a69f8ded0
Matyáš Strelec <matyasstrelec@gmail.com>
2022-05-01 18:13:07 +0200

    unnamed semaphores

--- proj2.c
+++ proj2.c
@@ -16,21 +16,20 @@
 #include <sys/stat.h>
 
 // Semaphore pointers
-sem_t *s_writeFile;
+sem_t *s_writeFile = NULL;
 
-sem_t *s_mutex;
-sem_t *s_oxyQueue;
-sem_t *s_hydroQueue;
+sem_t *s_mutex = NULL;
+sem_t *s_oxyQueue = NULL;
+sem_t *s_hydroQueue = NULL;
 
-sem_t *s_barrierMutex;
-sem_t *s_barrierTurnstile;
-sem_t *s_barrierTurnstile2;
+sem_t *s_barrierMutex = NULL;
+sem_t *s_barrierTurnstile = NULL;
+sem_t *s_barrierTurnstile2 = NULL;
 
-sem_t *s_maxMoleculeCheckO;
-sem_t *s_maxMoleculeCheckH;
+sem_t *s_maxMoleculeCheckO = NULL;
+sem_t *s_maxMoleculeCheckH = NULL;
 
-sem_t *s_moleculeCreated;
-
+sem_t *s_moleculeCreated = NULL;
 
 // Shared memory pointers
 int *maxMoleculeCount = NULL;
@@ -48,72 +47,91 @@ FILE *fp;
 
 // Function definition
 void initSempahores()
-{
-    s_writeFile = sem_open("/xstrel03/s_writeFile", O_CREAT | O_EXCL, 0666, 1);
+{    
+    sem_init(s_writeFile, 1, 1);
 
-    s_mutex = sem_open("/xstrel03/s_mutex", O_CREAT | O_EXCL, 0666, 1);
-    s_oxyQueue = sem_open("/xstrel03/s_oxyQueue", O_CREAT | O_EXCL, 0666, 0);
-    s_hydroQueue = sem_open("/xstrel03/s_hydroQueue", O_CREAT | O_EXCL, 0666, 0);
+    sem_init(s_mutex, 1, 1);
+    sem_init(s_oxyQueue, 1, 0);
+    sem_init(s_hydroQueue, 1, 0);
 
-    s_barrierMutex = sem_open("/xstrel03/s_barrierMutex", O_CREAT | O_EXCL, 0666, 1);
-    s_barrierTurnstile = sem_open("/xstrel03/s_barrierTurnstile", O_CREAT | O_EXCL, 0666, 0);
-    s_barrierTurnstile2 = sem_open("/xstrel03/s_barrierTurnstile2", O_CREAT | O_EXCL, 0666, 1);
+    sem_init(s_barrierMutex, 1, 1);
+    sem_init(s_barrierTurnstile, 1, 0);
+    sem_init(s_barrierTurnstile2, 1, 1);
 
-    s_maxMoleculeCheckO = sem_open("/xstrel03/s_maxMoleculeCheck", O_CREAT | O_EXCL, 0666, 1);
-    s_maxMoleculeCheckH = sem_open("/xstrel03/s_maxMoleculeCheck", O_CREAT | O_EXCL, 0666, 2);
-    s_moleculeCreated = sem_open("/xstrel03/s_moleculeCreated", O_CREAT | O_EXCL, 0666, 0);
+    sem_init(s_maxMoleculeCheckO, 1, 1);
+    sem_init(s_maxMoleculeCheckH, 1, 2);
+    sem_init(s_moleculeCreated, 1, 0);
 }
 
 void initSharedMemory()
 {
+    s_writeFile = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
+    s_mutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    s_oxyQueue = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    s_hydroQueue = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
+    s_barrierMutex = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    s_barrierTurnstile = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    s_barrierTurnstile2 = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
+    s_maxMoleculeCheckO  = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    s_maxMoleculeCheckH = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    s_moleculeCreated = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
     maxMoleculeCount = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    
+    moleculeCounter = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     sequenceCounter = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    
     oxygen = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
     hydrogen = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    
     barrierCount = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    moleculeCounter = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
 }
 
 void destroySemaphores()
 {
-    sem_close(s_writeFile);
-    sem_unlink("/xstrel03/s_writeFile");
+    sem_destroy(s_writeFile);
 
-    sem_close(s_mutex);
-    sem_unlink("/xstrel03/s_mutex");
+    sem_destroy(s_mutex);
+    sem_destroy(s_oxyQueue);
+    sem_destroy(s_hydroQueue);
 
-    sem_close(s_oxyQueue);
-    sem_unlink("/xstrel03/s_oxyQueue");
+    sem_destroy(s_barrierMutex);
+    sem_destroy(s_barrierTurnstile);
+    sem_destroy(s_barrierTurnstile2);
 
-    sem_close(s_hydroQueue);
-    sem_unlink("/xstrel03/s_hydroQueue");
-    
-    sem_close(s_barrierMutex);
-    sem_unlink("/xstrel03/s_barrierMutex");
-    
-    sem_close(s_barrierTurnstile);
-    sem_unlink("/xstrel03/s_barrierTurnstile");
-    sem_close(s_barrierTurnstile2);
-    sem_unlink("/xstrel03/s_barrierTurnstile2");
-
-    sem_close(s_maxMoleculeCheckO);
-    sem_unlink("/xstrel03/s_maxMoleculeCheckO");
-    sem_close(s_maxMoleculeCheckH);
-    sem_unlink("/xstrel03/s_maxMoleculeCheckH");
-    
-    sem_close(s_moleculeCreated);
-    sem_unlink("/xstrel03/s_moleculeCreated");
-
+    sem_destroy(s_maxMoleculeCheckO);
+    sem_destroy(s_maxMoleculeCheckH);
+    sem_destroy(s_moleculeCreated);
 }
 
 void destroySharedMemory()
 {
+    munmap(s_writeFile, sizeof(sem_t));
+
+    munmap(s_mutex, sizeof(sem_t));
+    munmap(s_oxyQueue, sizeof(sem_t));
+    munmap(s_hydroQueue, sizeof(sem_t));
+
+    munmap(s_barrierMutex, sizeof(sem_t));
+    munmap(s_barrierTurnstile, sizeof(sem_t));
+    munmap(s_barrierTurnstile2, sizeof(sem_t));
+
+    munmap(s_maxMoleculeCheckO, sizeof(sem_t));
+    munmap(s_maxMoleculeCheckH, sizeof(sem_t));
+    munmap(s_moleculeCreated, sizeof(sem_t));
+
     munmap(maxMoleculeCount, sizeof(int));
+
+    munmap(moleculeCounter, sizeof(int));
     munmap(sequenceCounter, sizeof(int));
+    
     munmap(oxygen, sizeof(int));
     munmap(hydrogen, sizeof(int));
+    
     munmap(barrierCount, sizeof(int));
-    munmap(moleculeCounter, sizeof(int));
 }
 
 void myPrint(const char *format, ...)
@@ -196,17 +214,17 @@ void oxygenProcess(int idO, int NO, int TI, int TB)
         myPrint("O %d: not enough H\n", idO);
         sem_post(s_maxMoleculeCheckO);
         // sem_post(s_mutex);
-        return;
+        exit(0);
     }
 
     sem_wait(s_oxyQueue);
+
+    barrierWait1();
 
     myPrint("O %d: creating molecule %d\n", idO, (*moleculeCounter));
 
     // Sleep <0, TB>
     usleep(((rand() % (TB + 1)) * 1000));
-
-    barrierWait1();
 
     // Molecule created    
     myPrint("O %d: molecule %d created\n", idO, (*moleculeCounter));
@@ -224,7 +242,7 @@ void oxygenProcess(int idO, int NO, int TI, int TB)
 
     sem_post(s_mutex);
 
-    return;
+    exit(0);
 }
 
 void hydrogenProcess(int idH, int NH, int TI)
@@ -262,7 +280,8 @@ void hydrogenProcess(int idH, int NH, int TI)
     {
         myPrint("H %d: not enough O or H\n", idH);
         sem_post(s_maxMoleculeCheckH);
-        return;
+
+        exit(0);
     }
 
     sem_wait(s_hydroQueue);
@@ -277,15 +296,15 @@ void hydrogenProcess(int idH, int NH, int TI)
 
     barrierWait2();
 
-    return;
+    exit(0);
 }
 
 // Main function
 int main(int argc, char const *argv[])
 {
     // Destroy shared memory preventively
+    destroySharedMemory();
     destroySemaphores();
-    // destroySharedMemory();
 
     // Get command line arguments
     if (argc != 5)
@@ -305,6 +324,9 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    initSharedMemory();
+    initSempahores();
+
     // Create file for output
     fp = fopen("proj2.out", "w");
     if (fp == NULL)
@@ -312,9 +334,6 @@ int main(int argc, char const *argv[])
         fprintf(stderr, "Problem creating file.\n");
         return 1;
     }
-
-    initSempahores();
-    initSharedMemory();
 
     setbuf(fp, NULL);
     setbuf(stdout, NULL);
@@ -362,9 +381,10 @@ int main(int argc, char const *argv[])
     // Wait for processes to finish
     while(wait(NULL) > 0);
     
-    destroySemaphores();
-    destroySharedMemory();
-
     fclose(fp);    
+    
+    destroySharedMemory();
+    destroySemaphores();
+
     return 0;
 }
